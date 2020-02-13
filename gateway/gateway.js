dc7f5c4dbccf65dd96fe9ae754dc1282e15960a8
TArch64 <tarasturchenko1551@gmail.com>
2020-02-13 18:54:49 +0200

    added test server

--- gateway/gateway.js
+++ gateway/gateway.js
@@ -1,6 +1,11 @@
 const { ServiceRoot } = require('./src');
 
-new ServiceRoot().run().catch(error => {
-    console.error(error);
-    process.exit(1);
-});
+// new ServiceRoot().run().catch(error => {
+//     console.error(error);
+//     process.exit(1);
+// });
+
+require('http').createServer((req, res) => {
+    res.write('Hello World!');
+    res.end();
+}).listen(2000);
